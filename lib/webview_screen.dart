02cbf0b7354705d7d97b23f7055f2c1db86bc44b
chisom <chisomekwuribe@gmail.com>
2022-02-14 17:30:37 +0100

    Changed visibility widget

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -42,10 +42,9 @@ class _WebviewScreenState extends State<WebviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Visibility(
-        visible: isGranted,
-        child: InAppWebView(
-          initialData: InAppWebViewInitialData(data: """
+      body: isGranted
+          ? InAppWebView(
+              initialData: InAppWebViewInitialData(data: """
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -56,65 +55,58 @@ class _WebviewScreenState extends State<WebviewScreen> {
 
 <script src="https://widget.dojah.io/widget.js"></script>
 <script>
-            const options = {
-                app_id: "${widget.appId}",
-                p_key: "${widget.publicKey}",
-                onSuccess: function (response) {
-                 window.flutter_inappwebview.callHandler('onSuccessCallback', response)
-                },
-                onError: function (err) {
-                  window.flutter_inappwebview.callHandler('onErrorCallback', error)
-                },
-                onClose: function () {
-                  window.flutter_inappwebview.callHandler('onCloseCallback', 'close')
-                }
-            }
+          const options = {
+              app_id: "${widget.appId}",
+              p_key: "${widget.publicKey}",
+              onSuccess: function (response) {
+               window.flutter_inappwebview.callHandler('onSuccessCallback', response)
+              },
+              onError: function (err) {
+                window.flutter_inappwebview.callHandler('onErrorCallback', error)
+              },
+              onClose: function () {
+                window.flutter_inappwebview.callHandler('onCloseCallback', 'close')
+              }
+          }
 
-              const connect = new Connect(options);
-              connect.setup();
-              connect.open();
-        </script>
+            const connect = new Connect(options);
+            connect.setup();
+            connect.open();
+      </script>
 </body>
 </html>
-                    """),
-          initialOptions:
-              InAppWebViewGroupOptions(crossPlatform: InAppWebViewOptions()),
-          onWebViewCreated: (InAppWebViewController controller) {
-            _webViewController = controller;
+                  """),
+              initialOptions: InAppWebViewGroupOptions(
+                  crossPlatform: InAppWebViewOptions()),
+              onWebViewCreated: (InAppWebViewController controller) {
+                _webViewController = controller;
 
-            _webViewController?.addJavaScriptHandler(
-                handlerName: 'onSuccessCallback',
-                callback: (response) {
-                  widget.success(response);
-                });
+                _webViewController?.addJavaScriptHandler(
+                    handlerName: 'onSuccessCallback',
+                    callback: (response) {
+                      widget.success(response);
+                    });
 
-            _webViewController?.addJavaScriptHandler(
-                handlerName: 'onCloseCallback',
-                callback: (response) {
-                  // widget.onCloseCallback!(response);
-                  if (response.first == 'close') {
-                    Navigator.pop(context);
-                  }
-                });
+                _webViewController?.addJavaScriptHandler(
+                    handlerName: 'onCloseCallback',
+                    callback: (response) {
+                      // widget.onCloseCallback!(response);
+                      if (response.first == 'close') {
+                        Navigator.pop(context);
+                      }
+                    });
 
-            _webViewController?.addJavaScriptHandler(
-                handlerName: 'onErrorCallback',
-                callback: (error) {
-                  widget.error(error);
-                });
-          },
-          onConsoleMessage: (controller, consoleMessage) {
-            print(consoleMessage.message);
-          },
-        ),
-        replacement: Center(
-          child: Column(
-            children: const [
-              Text("Please grant camera permissions"),
-            ],
-          ),
-        ),
-      ),
+                _webViewController?.addJavaScriptHandler(
+                    handlerName: 'onErrorCallback',
+                    callback: (error) {
+                      widget.error(error);
+                    });
+              },
+              onConsoleMessage: (controller, consoleMessage) {
+                print(consoleMessage.message);
+              },
+            )
+          : const Center(child: Text("Please grant camera permissions")),
     );
   }
 }
