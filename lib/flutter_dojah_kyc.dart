8a337eb4f8f51bd27cb1019cf5545df35071061b
Samuel Adekunle <samuelbeebest@gmail.com>
2024-05-30 06:36:48 +0100

    removal of unnecessary package

--- lib/flutter_dojah_kyc.dart
+++ lib/flutter_dojah_kyc.dart
@@ -11,11 +11,9 @@ class DojahKYC {
   final String? referenceId;
   final Map<String, dynamic>? userData;
   final Map<String, dynamic>? metaData;
-    final Map<String, dynamic>? govData;
+  final Map<String, dynamic>? govData;
   final Map<String, dynamic>? config;
   final Function(dynamic)? onCloseCallback;
-
-  
 
   DojahKYC({
     required this.appId,
@@ -50,9 +48,8 @@ class DojahKYC {
           success: (result) {
             onSuccess!(result);
           },
-
           close: (close) {
-              onClose!(close);
+            onClose!(close);
           },
           error: (error) {
             onError!(error);
